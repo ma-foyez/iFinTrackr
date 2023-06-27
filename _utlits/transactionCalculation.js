d6434ff052f1d549f5b6228ab0795bb636a45380
ma-foyez <mafoyez.web@gmail.com>
2023-06-28 02:10:16 +0600

    optimized codes for more readable

--- _utlits/transactionCalculation.js
+++ _utlits/transactionCalculation.js
@@ -1,45 +1,62 @@
 const Profile = require("../models/peopleModal");
 
 const managePeopleCalculation = async (personaId, amount, transactionType) => {
+    // Retrieve user profile from the database
     const getUserById = await Profile.findOne({ _id: personaId });
     const { total_liabilities, total_payable, due_liabilities, due_payable } = getUserById;
 
+    // Initialize variables with profile data
     let TotalLiabilities = total_liabilities;
     let totalPayable = total_payable;
     let dueLiabilities = due_liabilities;
     let duePayable = due_payable;
 
-    if (transactionType == "liabilities") {
-        TotalLiabilities += amount; // Previous Total Liabilities + Amount
-        if(duePayable > 0){
+    // Calculate values based on transaction type
+    if (transactionType === "liabilities") {
+        // Increase total liabilities by the amount
+        TotalLiabilities += amount;
+
+        // Handle existing due payable
+        if (duePayable > 0) {
             const newPayable = duePayable - amount;
-            if(newPayable < 0){
+
+            // If new payable is negative, update due liabilities and set due payable to 0
+            if (newPayable < 0) {
                 dueLiabilities += Math.abs(newPayable);
                 duePayable = 0;
-            }else{
+            } else {
+                // Otherwise, update due payable and set due liabilities to 0
                 duePayable = Math.abs(newPayable);
                 dueLiabilities = 0;
             }
-        }else{
-            // payable logics
+        } else {
+            // If no existing due payable, increase due liabilities by the amount
             dueLiabilities += amount;
         }
     } else {
-        totalPayable += amount; // Previous Total Payable + Amount
-        if(dueLiabilities > 0){
+        // Increase total payable by the amount
+        totalPayable += amount;
+
+        // Handle existing due liabilities
+        if (dueLiabilities > 0) {
             const newDueLiabilities = dueLiabilities - amount;
-            if(newDueLiabilities < 0){
+
+            // If new due liabilities is negative, update due payable and set due liabilities to 0
+            if (newDueLiabilities < 0) {
                 duePayable += Math.abs(newDueLiabilities);
                 dueLiabilities = 0;
-            }else{
+            } else {
+                // Otherwise, update due liabilities and set due payable to 0
                 dueLiabilities = Math.abs(newDueLiabilities);
                 duePayable = 0;
             }
-        }else{
+        } else {
+            // If no existing due liabilities, increase due payable by the amount
             duePayable += amount;
         }
     }
 
+    // Return the updated calculation values
     return {
         TotalLiabilities,
         totalPayable,
@@ -48,4 +65,4 @@ const managePeopleCalculation = async (personaId, amount, transactionType) => {
     };
 };
 
-module.exports = { managePeopleCalculation }
+module.exports = { managePeopleCalculation };
